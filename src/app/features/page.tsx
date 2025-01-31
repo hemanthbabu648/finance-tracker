026300b2cca1baffcefb4f4bec2e842cc979c1de
hemanthbabu648 <hemanth170648@gmail.com>
2025-01-31 10:23:38 +0530

    FT-4: updated features page with header and footer

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/home/Footer";
+import Header from "@/components/home/Header";
 import { features } from "@/constants/home";
 import { SimpleGrid, ThemeIcon } from "@mantine/core";
 
@@ -18,25 +20,29 @@ const renderFeatures = features.map((feature) => (
 export default function FeaturesPage() {
 
     return (
-        <main className="max-w-7xl mx-auto my-5 px-4 text-center">
-            <h1 className="text-2xl lg:text-3xl xl:text-4xl font-semibold">Explore Powerful Features for Smarter Financial Management</h1>
-            <p className="text-base md:text-lg lg:text-xl">
-                Discover how our intuitive tools help you track spending, set goals, and make confident financial decisions—all in one place.
-            </p>
-            <p className="hidden lg:block py-10 lg:mt-5">
-                Track your spending, set savings goals, and plan for the future—all in one place. Our intuitive personal finance tracker helps you manage your money with ease, so you can make smarter financial decisions and achieve your goals faster.
-            </p>
-            <div className="mt-10 sm:mt-20 lg:mt-30">
-                <SimpleGrid
+        <div className="flex flex-col min-h-screen justify-between">
+            <Header />
+            <main className="max-w-7xl mx-auto my-5 px-4 text-center">
+                <h1 className="text-2xl lg:text-3xl xl:text-4xl font-semibold">Explore Powerful Features for Smarter Financial Management</h1>
+                <p className="text-base md:text-lg lg:text-xl">
+                    Discover how our intuitive tools help you track spending, set goals, and make confident financial decisions—all in one place.
+                </p>
+                <p className="hidden lg:block py-10 lg:mt-5">
+                    Track your spending, set savings goals, and plan for the future—all in one place. Our intuitive personal finance tracker helps you manage your money with ease, so you can make smarter financial decisions and achieve your goals faster.
+                </p>
+                <div className="mt-10 sm:mt-20 lg:mt-30">
+                    <SimpleGrid
 
-                    cols={{ base: 1, sm: 2, md: 3 }}
-                    spacing={{ base: 'xl', md: 50 }}
-                    verticalSpacing={{ base: 'xl', md: 50 }}
-                >
-                    {renderFeatures}
-                </SimpleGrid>
-            </div>
+                        cols={{ base: 1, sm: 2, md: 3 }}
+                        spacing={{ base: 'xl', md: 50 }}
+                        verticalSpacing={{ base: 'xl', md: 50 }}
+                    >
+                        {renderFeatures}
+                    </SimpleGrid>
+                </div>
 
-        </main>
+            </main>
+            <Footer />
+        </div>
     )
 }
