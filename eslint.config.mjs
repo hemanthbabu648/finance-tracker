4a158248404aecc4002f121e373e013c7ad38fb0
hemanthbabu648 <hemanth170648@gmail.com>
2025-02-27 19:48:43 +0530

    FT: add eslint and prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),{
+    plugins: ["import"],
+    rules: {
+      "import/no-duplicates": 2,
+      "import/order": [
+        error,
+        {
+          groups: [
+            "builtin",   // Node.js built-in modules
+            "external",  // External modules from npm
+            "internal",  // Internal modules (e.g., alias imports)
+            ["parent", "sibling", "index"], // Relative imports
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "import/newline-after-import": 2,
+    }
+  }
 ];
 
 export default eslintConfig;
