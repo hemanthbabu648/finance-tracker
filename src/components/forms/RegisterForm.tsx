f50949f924c9ea68f55cbebf56a50a604734fcfc
hemanthbabu648 <hemanth170648@gmail.com>
2025-03-06 13:01:29 +0530

    FT: bug-registration: password, message and redirect url

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Group, Progress } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { useDisclosure } from '@mantine/hooks'
 import Link from 'next/link'
@@ -15,12 +16,69 @@ import Modal from '../commons/Modal'
 import PasswordInput from '../commons/PasswordInput'
 import TextInput from '../commons/TextInput'
 
+const requirements = [
+    { re: /[0-9]/, label: 'Includes number' },
+    { re: /[a-z]/, label: 'Includes lowercase letter' },
+    { re: /[A-Z]/, label: 'Includes uppercase letter' },
+    { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Includes special symbol' },
+];
 
+function getStrength(password: string) {
+    if (password.length < 5) {
+        return 10;
+    }
+
+    let multiplier = password.length > 5 ? 0 : 1;
+
+    requirements.forEach((requirement) => {
+        if (!requirement.re.test(password)) {
+            multiplier += 1;
+        }
+    });
+
+    return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
+}
+
+function getStrengthColor(strength: number) {
+    switch (true) {
+        case strength < 30:
+            return 'red';
+        case strength < 50:
+            return 'orange';
+        case strength < 70:
+            return 'yellow';
+        default:
+            return 'teal';
+    }
+}
+
+const validatePassword = (value: string) => {
+    if (!value || value.length < 1) {
+        return 'Password is required.';
+    }
+
+    if (value.length < 8) {
+        return 'Password must be at least 8 characters long.';
+    }
+
+    if (value.length > 16) {
+        return 'Password must not exceed 16 characters.';
+    }
+
+    const errors = requirements
+        .filter(req => !req.re.test(value))
+        .map(req => req.label);
+
+    return errors.length > 0 ? `Password must: ${errors.join(', ')}.` : null;
+};
 
 const RegisterForm: React.FC = () => {
     const router = useRouter()
-    const [loading, setLoading] = React.useState(false)
+    const [loading, setLoading] = React.useState<boolean>(false)
     const [opened, { open, close }] = useDisclosure(false);
+    const [watchPassword, setWatchPassword] = React.useState<string>('')
+    const strength = getStrength(watchPassword);
+    const color = getStrengthColor(strength);
 
     const form = useForm({
         mode: 'uncontrolled',
@@ -34,15 +92,19 @@ const RegisterForm: React.FC = () => {
             agreement: false,
         },
         validate: {
-            firstName: (value) => (value.length >= 1 ? null : 'First name must be at least 1 character'),
-            lastName: (value) => (value.length >= 1 ? null : 'Last name must be at least 1 character'),
-            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-            password: (value) => (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(value) ? null : 'Password must be at least 8 characters'),
-            confirmPassword: (value, values) => (value === values.password ? null : 'Passwords do not match'),
-            terms: (value) => (value ? null : 'You must agree to the terms and conditions'),
-            agreement: (value) => (value ? null : 'You must agree to the agreement'),
+            firstName: (value) => (value.length >= 1 ? null : 'First name is required.'),
+            lastName: (value) => (value.length >= 1 ? null : 'Last name is required.'),
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email.'),
+            password: (value) => validatePassword(value),
+            confirmPassword: (value, values) => (!value ? "Confirm Password is required." : value === values.password ? null : 'Passwords not matched.'),
+            terms: (value) => (value ? null : 'You must agree to the terms and privacy policy.'),
+            agreement: (value) => (value ? null : 'You must agree to the agreement.'),
         },
 
+    })
+
+    form.watch('password', (value) => {
+        setWatchPassword(value.value)
     })
 
     const handleSubmit = async (values: typeof form.values) => {
@@ -110,13 +172,25 @@ const RegisterForm: React.FC = () => {
 
                 <PasswordInput
                     label="Password"
-                    placeholder="Please enter your password"
+                    placeholder="YourPassword@123"
                     variant="filled"
                     withAsterisk
                     size="md"
                     key={form.key('password')}
                     {...form.getInputProps('password')}
                 />
+
+                <Group grow gap={5} mt="xs">
+                    <Progress
+                        size="xs"
+                        color={color}
+                        value={watchPassword.length > 0 ? 100 : 0}
+                        transitionDuration={0}
+                    />
+                    <Progress size="xs" color={color} transitionDuration={0} value={strength < 30 ? 0 : 100} />
+                    <Progress size="xs" color={color} transitionDuration={0} value={strength < 50 ? 0 : 100} />
+                    <Progress size="xs" color={color} transitionDuration={0} value={strength < 70 ? 0 : 100} />
+                </Group>
 
                 <PasswordInput
                     label="Confirm Password"
@@ -130,37 +204,39 @@ const RegisterForm: React.FC = () => {
 
                 <div className="flex gap-2 ">
                     <Checkbox
-                        label=""
+                        label="Terms & Privacy Policy"
+                        description={<span className="!text-gray">
+                            By checking this box, I agree to the{" "}
+                            <Link href="/privary" className="underline text-navy-blue-300">
+                                Terms of Service
+                            </Link>{" "}
+                            and{" "}
+                            <Link href="/privary" className="underline text-navy-blue-300">
+                                Privacy Policy
+                            </Link>
+                            .
+                        </span>}
                         variant="filled"
                         key={form.key('terms')}
                         {...form.getInputProps('terms')}
                     />
-                    <p className="text-gray">
-                        By checking this box, I agree to the{" "}
-                        <Link href="/privary" className="hover:underline text-navy-blue-300">
-                            Terms of Service
-                        </Link>{" "}
-                        and{" "}
-                        <Link href="/privary" className="hover:underline text-navy-blue-300">
-                            Privacy Policy
-                        </Link>
-                        .
-                    </p>
+
                 </div>
                 <div className="flex gap-2 ">
                     <Checkbox
-                        label=""
+                        label="Agreement"
+                        description={<span className="!text-gray">
+                            By checking this box, I consent by electronic signature to receive
+                            emails from Finomic [Radiant Ways], LLC and its affiliates made by any means or
+                            technology for informational, at the
+                            email I provided above. I understand that my consent to such emails is
+                            not required to use the services of Finomic [Radiant Ways], LLC.
+                        </span>}
                         variant="filled"
                         key={form.key('agreement')}
                         {...form.getInputProps('agreement')}
                     />
-                    <p className="text-gray">
-                        By checking this box, I consent by electronic signature to receive
-                        emails from Radiant Ways, LLC and its affiliates made by any means or
-                        technology for informational, marketing, or any other purposes at the
-                        email I provided above. I understand that my consent to such emails is
-                        not required to use the services of Radiant Ways, LLC.
-                    </p>
+
                 </div>
                 <p className="text-sm sm:text-base text-gray-500 mt-2">
                     Already have an account?{" "}
@@ -191,14 +267,18 @@ const RegisterForm: React.FC = () => {
                 withCloseButton={false}
                 size="auto"
             >
-                <div className="flex flex-col items-center justify-center p-6">
-                    <h1 className="text-3xl font-bold mb-4">
+                <div className="flex flex-col items-center justify-center p-6 space-y-2">
+                    <h1 className="text-3xl font-bold">
                         Welcome to <span className="text-primary-blue-light">Finomic</span>! 🎉
                     </h1>
 
-                    <p className="text-lg text-center max-w-lg mb-6">
+                    <p className="text-lg text-center max-w-lg text-slate-500">
                         You&apos;ve successfully registered. We&apos;re excited to have you on board! Explore all the features we offer and start your journey with us. If you have any questions, feel free to check out our help center or reach out to our support team.
                     </p>
+
+                    <p className='py-1 text-xl text-primary font-semibold'>[Radiant Ways].</p>
+
+                    <p className='py-2'>Check your email for the verification link.</p>
 
                     <Button
                         size="md"
